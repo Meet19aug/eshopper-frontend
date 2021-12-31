e83db2d9d98633edc560dfdc62ebd198fda4446b
Meet Patel <meetpatel19aug@gmail.com>
2021-12-31 17:30:14 +0530

    Initialize the work of overlay navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./components/layout/Header.js"
+import {BrowserRouter as Router} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+    <Header/>
+    </Router>
+  );
+}
+
+export default App;

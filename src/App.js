1cc496632d9ae8fc1ad965acb820e358141342e3
Meet Patel <meetpatel19aug@gmail.com>
2022-01-12 13:34:53 +0530

    Scroll to top in redirection is working properly.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useLayoutEffect } from "react";
 import './App.css';
 import Header from "./components/layout/Header/Header.js";
 import Footer from "./components/layout/Footer/Footer.js";
@@ -6,7 +6,9 @@ import Home from "./components/Home/Home.js";
 import Products from "./components/Product/Products.js";
 import ProductDetails from "./components/Product/ProductDetails.js"
 import Search from "./components/Product/Search.js" 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom'
+
 import WebFont from "webfontloader"; 
 import LoginSignUp from "./components/User/LoginSignUp";
 import store from "./store"
@@ -14,6 +16,14 @@ import { loadUser } from "./actions/userAction";
 import UserOptions from "./components/layout/Header/UserOptions.js"
 import { useSelector } from "react-redux";
 
+//https://stackoverflow.com/questions/70193712/how-to-scroll-to-top-on-route-change-with-react-router-dom-v6#:~:text=You%20can%20use%20the%20above,()%3B%20%7D%20%7D%2C%20%5BasPath%5D)%3B
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+} 
 
 function App() {
 
@@ -31,6 +41,7 @@ function App() {
   return (
     <>
     <Router>
+    <Wrapper>
       <Header/>
       {isAuthenticated && <UserOptions user={user}/>}
       <Routes>
@@ -42,7 +53,7 @@ function App() {
         <Route exact path="/login" element={<LoginSignUp/>}/>
       </Routes>
       <Footer/>
-
+      </Wrapper>
     </Router>
     </>
   );
